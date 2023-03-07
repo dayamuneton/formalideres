00086ea70bc18416afc36279a1c909eab3903d67
agredo-alejo <agredoalejo@gmail.com>
2023-03-07 18:58:11 -0500

    changed andrea y daya photo

--- components/home/somos.tsx
+++ components/home/somos.tsx
@@ -3,23 +3,23 @@ import React from "react";
 
 function Somos() {
    return (
-      <div>
+      <div className="flex flex-col items-center max-w-4xl overflow-hidden">
          <span>
-            <p className="text-gray-600 text-2xl font-bold mt-8">
+            <p className="mt-8 text-2xl font-bold text-gray-600">
                Hola ! Somos
             </p>
-            <p className="font-haviland text-4xl text-blue-600 ml-10">
+            <p className="ml-10 text-4xl text-blue-600 font-haviland">
                Andrea Virdo y Daya Muneton
             </p>
          </span>
-         <div className="w-full max-w-4xl flex items-center flex-col md:flex-row">
+         <div className="flex flex-col items-center justify-center w-full md:flex-row">
             <Image
-               src="/TempDaya.webp"
+               src="/AndreaYDaya.png"
                alt=""
                fill
-               className="!relative w-full"
+               className="!relative flex !max-w-[50%]"
             />
-            <div className="text-sm md:my-auto pl-2 max-w-[33ch] w-[70%] leading-4 mt-6">
+            <div className="flex flex-col items-center text-sm md:my-auto pl-2 max-w-[33ch] w-full leading-4 mt-6 ">
                <p>
                   Andrea psicóloga, con una maestría en Salud mental (Nova
                   Southeastern University) y Daya artista durante siete años
--- components/home/workshop.tsx
+++ components/home/workshop.tsx
@@ -3,24 +3,24 @@ import SouthIcon from "@mui/icons-material/South";
 
 function Workshop() {
    return (
-      <div className="relative w-full pt-4 flex flex-col items-center">
-         <span className="bg-black text-white text-xl absolute left-4">
+      <div className="relative flex flex-col items-center w-full pt-4">
+         <span className="absolute text-xl text-white bg-black left-4">
             Arte, ciencia y Tecnología
          </span>
          <SouthIcon className="text-blue-800" sx={{ fontSize: "4rem" }} />
          <div className="bg-red-500 h-2 w-[90vw] my-8"></div>
-         <div className="flex flex-col relative items-center">
-            <p className="text-blue-600 rotate-[-5deg] text-5xl left-[-4rem] absolute">
+         <div className="relative flex flex-col items-center">
+            <p className="text-blue-600 rotate-[-5deg] text-5xl left-[-7rem] absolute font-cutive">
                Workshop
             </p>
-            <h2 className="text-gray-600 text-5xl font-bold mt-12">
+            <h2 className="mt-12 text-5xl font-bold text-gray-600">
                Forma líderes
             </h2>
-            <p className="flex bg-yellow w-fit text-blue-600 text-xl">
+            <p className="flex text-xl text-blue-600 bg-yellow w-fit">
                Que te lleven a la cima
             </p>
          </div>
-         <div className="w-[90vw] max-w-xl font-thin text-sm flex flex-col  gap-3 mt-4">
+         <div className="w-[90vw] max-w-md font-thin text-sm flex flex-col  gap-3 mt-4">
             <p>
                Cuando piensas en crecimiento del negocio muchas veces te enfocas
                en las barreras de procesos, económicas, infraestructura o
@@ -43,7 +43,7 @@ function Workshop() {
                La forma más fácil de ser un equipo altamente pro-activo y
                eficiente es logrando tener coherencia emocionalmente.
             </p>
-            <p className="bg-yellow text-blue-600 font-medium px-1">
+            <p className="px-1 font-medium text-blue-600 bg-yellow">
                YEP! Lo leíste correctamente!! Los beneficios de conocer tus
                emociones, tiene que ver con tener resultados, mejorar tu
                servicio o tu productividad, mejorar la comunicación y aumentar
